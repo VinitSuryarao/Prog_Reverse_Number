a2f348e01ce6adcde48f7dda168ff6dc527a1cf6
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:02:03 +0530

    Add files via upload

---
+++ Reverse_Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Reverse Number
+        // Number = 12345
+        // Reverse = 54321
+        static void Main(string[] args)
+        {
+            int n = 12345, rev = 0;
+            
+            while(n!=0)
+            {
+                rev = rev * 10;
+                rev = rev + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine(rev);
+        
+
+        }
+    }
+
+
